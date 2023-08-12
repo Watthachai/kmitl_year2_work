3bd9b099bba3e6a4b03ce238fc16afa70cd436e2
Watthachai Taechalue <wattchaichai@gmail.com>
2023-08-12 17:37:28 +0700

    <info> : update activities 3 on #OOD subject Head: Linklist

--- DSA/week5/65015137.py
+++ DSA/week5/65015137.py
@@ -30,7 +30,7 @@ s.push('D')
 s.push('E')
 s.push('F')
 
-print(s.items)
+print("\nActivities 1 \n",s.items)
 
 for i in range(len(s.items)):
     print('Remove:', s.pop(), 'success!.', 'empty yet?', s.isEmpty())
@@ -68,33 +68,64 @@ q.enQueue('D')
 q.enQueue('E')
 q.enQueue('F')
 
-print('\nAdded!', q.items, '\nTotal size :',len(q.items))
+print('\n\n#Activities 2 \nAdded!', q.items, '\nTotal size :',len(q.items))
 
 
-""" ACTIVITY 3
+"""
+!TODO ACTIVITY 3
 ให้นักศึกษาเขียนโปรแกรมสร้าง Link List แล้ว เก็บข้อมูล ABCDEF 
 หลังจากนั้นให้ แทรกข้อมูล XYZ ระหว่าง C,D
 """
 
-class node:
-    def __init__(self, data, next = None):
+class Node:
+    def __init__(self, data, next=None):
         self.data = data
-        if next == None:
+        
+        if next is None:
             self.next = None
         else:
-            self.next = None
+            self.next = next
 
-class list:
+class List:
     def __init__(self):
         self.head = None
-        
+
     def append(self, data):
-        
-        p = node(data)
-        if self.head == None:
-            self.head = p
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
         else:
-            t = self.head
-            while t.next != None:
-                t = t.next
-            t.next = p
+            s_now = self.head
+            while s_now.next:
+                s_now = s_now.next
+            s_now.next = new_node
+    
+    def node_print(self):
+        elements = []
+        s_now_node = self.head
+        while s_now_node:
+            elements.append(s_now_node.data)
+            s_now_node = s_now_node.next
+        return elements
+
+    def insert_after(self, target_data, data):
+        if self.head is None:
+            return
+        s_now = self.head
+        while s_now and s_now.data != target_data:
+            s_now = s_now.next
+        new_node = Node(data)
+        new_node.next = s_now.next
+        s_now.next = new_node
+
+ll = List()
+for i in "ABCDEF":
+    ll.append(i)
+    
+print("\n\n#Activities 3 \nList:", ll.node_print())
+
+ll.insert_after("C", "X")
+ll.insert_after("X", "Y")
+ll.insert_after("Y", "Z")
+
+print("Insert LL After", ll.node_print())
