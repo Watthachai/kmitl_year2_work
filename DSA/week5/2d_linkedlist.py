f2dd5ee7a3c2cfa442fb2157431867fe59621f3b
Watthachai Taechalue <wattchaichai@gmail.com>
2023-08-14 23:06:42 +0700

    <info> : update activities 3 on #OOD subject Head: Linklist <TODO: 2D Linklist

---
+++ DSA/week5/2d_linkedlist.py
@@ -0,0 +1,20 @@
+print("\n\n###################### 2D Linklist #####################")
+class Node:
+    def __init__(self, next=None, prev=None, data=None):
+        
+        #for next node
+        self.next = next
+
+        #ref to prev node
+        self.prev = prev
+        self.data = data
+        
+
+class LinkedList_2D:
+    None
+
+class priNode:
+    None
+
+class secNode:
+    None
