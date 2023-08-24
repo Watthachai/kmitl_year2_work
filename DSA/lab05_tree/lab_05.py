2c584aaaab1c8e57a5b17831210625a6a2d6ca3c
Watthachai Taechalue <wattchaichai@gmail.com>
2023-08-24 18:08:26 +0700

    <Feature> : update some files

--- DSA/lab05_tree/lab_05.py
+++ DSA/lab05_tree/lab_05.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Node:
     def __init__(self, data):
         self.left = None
@@ -19,34 +20,52 @@ class Node:
                     
             else: 
                 self.data = data
-                
-    def inOrder(self):
-        if self.left:
-            self.left.inOrder()
-        print(self.data, end=" ")
-        if self.right:
-            self.right.inOrder()
-
-    def preOrder(self):
-        print(self.data, end=" ")
-        if self.left:
-            self.left.preOrder()
-        if self.right:
-            self.right.preOrder()
-
-    def postOrder(self):
-        if self.left:
-            self.left.postOrder()
-        if self.right:
-            self.right.postOrder()
-        print(self.data, end=" ")
-
     def PrintTree(self):
         if self.left:
             self.left.PrintTree()
         print(self.data),
         if self.right:
-            self.right.PrintTree()
+            self.right.PrintTree()    
+                
+    def inOrderTraversal(self, root):
+        res = []
+        if root:
+            res = self.inOrderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inOrderTraversal(root.right)
+        return res
+
+    def preOrderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.preOrderTraversal(root.left)
+            res = res + self.preOrderTraversal(root.right)
+        return res
+
+    def postOrderTraversal(self, root):
+        res = []
+        if root:
+            res = self.postOrderTraversal(root.left)
+            res = res + self.postOrderTraversal(root.right)
+            res.append(root.data)
+        return res
+
+        
+    def breathFirstTraversal(self, root):
+        res = []
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+            res.append(node.data)
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return res
             
 root = Node(15)
 
@@ -62,14 +81,9 @@ root.insert(34)
 root.insert(19)
 root.insert(2)
 
+root.PrintTree()
 
-print("Inorder traversal:")
-root.inOrder()
-print()
-
-print("Preorder traversal:")
-root.preOrder()
-print()
-
-print("Postorder traversal:")
-root.postOrder()
+print(f"Inorder traversal: {root.inOrderTraversal(root)}")
+print(f"Preorder traversal: {root.preOrderTraversal(root)}")
+print(f"Postorder traversal: {root.postOrderTraversal(root)}")
+print(f"Level-order traversal: {root.breathFirstTraversal(root)}")
