8e42930e4342a2adc351d06a385d0a82c55d9948
Watthachai Taechalue <wattchaichai@gmail.com>
2023-09-23 16:47:09 +0700

    <Feature> : update some files

---
+++ Web/week8/my-app/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className='App'>
+        <h1 className='txtHeader'>โหวตอาหาร</h1>
+        
+      </div>
+    );
+  }
+}
+export default App;
