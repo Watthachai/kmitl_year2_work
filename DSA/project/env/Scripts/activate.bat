904ce2aaeb4446ee0f5508b77bfc87ebb6f7cd26
Watthachai Taechalue <wattchaichai@gmail.com>
2023-10-08 15:23:23 +0700

    <INFO> : Rework files

--- DSA/project/env/Scripts/activate.bat
+++ DSA/project/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\watthachai\OneDrive\kmitl_year2_work\DSA\project\env
+set VIRTUAL_ENV=C:\Users\wattc\OneDrive\kmitl_year2_work\DSA\project\env
 
 if not defined PROMPT set PROMPT=$P$G
 
