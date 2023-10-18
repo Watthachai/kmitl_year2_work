bbb2bc086e0d991be462d698397a816330d3ffbf
Watthachai Taechalue <wattchaichai@gmail.com>
2023-10-18 09:53:38 +0700

    <INFO> : Edit Jobs.py files

--- DSA/jobs.py
+++ DSA/jobs.py
@@ -2,71 +2,73 @@ class Queue:
     def __init__(self):
         self.queue = []
 
-    def addJob(self, ID, jobName):
-        # เพิ่มงานเข้าคิว โดยตรวจสอบสิทธิของพนักงาน
-        # ID ที่ 100-199 มีสิทธิสูงสุด
-        # 200-299 มีสิทธิรองสูงสุด
-        # 300-399 ...
-        # 800-899 มีสิทธิกอนกลุมต่ำสุด
-        # 900-999 มีสิทธิต่ำสุด
-        priority = int(ID) // 100
-        if priority < 1 or priority > 9:
-            print("Invalid priority")
-            return
-        self.queue.append((ID, jobName, priority))
-        print(f"Job submitted for ID {ID}")
+    def enqueue(self, job):
+        self.queue.append(job)
 
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    def size(self):
+        return len(self.queue) 
+
+class Job:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Company:
+    def __init__(self):
+        self.queues = [Queue() for _ in range(10)]
+
+    def addJob(self, id, jobName):
+        job = Job(id, jobName)
+        self.queues[int(id) // 100].enqueue(job)
+        print("Job submitted.")
+
+    def getNextJob(self):
+        for queue in self.queues:
+            if queue.size() > 0:
+                job = queue.dequeue()
+                print(f"ID : {job.id} Job : {job.name} starts running.")
+                break
+
+    def cancelJob(self, id, jobName):
+        for job in self.queues[int(id) // 100].queue:
+            if job.name == jobName:
+                self.queues[int(id) // 100].queue.remove(job)
+                print(f"ID : {id} Job: {jobName} has been cancelled.")
+                break
+
+    def urgentJob(self, id, jobName):
+        for job in self.queues[int(id) // 100].queue:
+            if job.name == jobName:
+                self.queues[int(id) // 100].queue.remove(job)
+                self.queues[0].enqueue(job)
+                print(f"ID : {id} Job: {jobName} has first priority.")
+                break
     def listJobs(self):
-        # แสดงรายการงานที่รอในคิว
-        self.queue.sort(key=lambda x: x[2])
-        print("ID\tJob Name")
-        for job in self.queue:
-            print(f"{job[0]}\t{job[1]}")
+            for i, queue in enumerate(self.queues):
+                if queue.size() > 0:
+                    print(f"Jobs in queue {i}:")
+                    for job in queue.queue:
+                        print(f"ID: {job.id}, Job Name: {job.name}")
+                        
 
-    def runJob(self):
-        # รันงานที่มีสิทธิสูงสุด
-        self.queue.sort(key=lambda x: x[2])
-        if self.queue:
-            ID, jobName, _ = self.queue.pop(0)
-            print(f"ID: {ID} Job: {jobName} starts running.")
-        else:
-            print("No jobs in the queue.")
-
-    def cancelJob(self, ID, jobName):
-        # ยกเลิกงานที่รอในคิว
-        for job in self.queue:
-            if job[0] == ID and job[1] == jobName:
-                self.queue.remove(job)
-                print(f"ID: {ID} Job: {jobName} has been cancelled.")
-                return
-        print(f"No matching job found for ID {ID} and job {jobName}.")
-
-    def urgentJob(self, ID, jobName):
-        # กำหนดงานที่มีสิทธิต่ำสุดเป็นงานแรกในคิว
-        for job in self.queue:
-            if job[0] == ID and job[1] == jobName:
-                job = self.queue.pop(self.queue.index(job))
-                self.queue.insert(0, job)
-                print(f"ID: {ID} Job: {jobName} has first priority.")
-                return
-        print(f"No matching job found for ID {ID} and job {jobName}.")
-
-# สร้างคิว
-queue = Queue()
-
-# รับคำสั่งและดำเนินการตามคำสั่ง
+queue = Company()
 while True:
-    command = input()
+    command = input("Enter Command: ")
     if command == "A":
-        ID, jobName = input().split()
+        ID, jobName = input("Enter ID and Job Name: ").split()
         queue.addJob(ID, jobName)
     elif command == "L":
         queue.listJobs()
     elif command == "R":
         queue.runJob()
     elif command == "C":
-        ID, jobName = input().split()
+        ID, jobName = input("Enter ID and Job Name: ").split()
         queue.cancelJob(ID, jobName)
     elif command == "U":
-        ID, jobName = input().split()
+        ID, jobName = input("Enter ID and Job Name: ").split()
         queue.urgentJob(ID, jobName)
